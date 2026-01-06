1021c2cc6acb4fa89583aff36e4a6b706220b51f
mhm <harshil.m@bitontree.com>
2026-01-06 11:12:13 +0530

    feat : Add profile page

--- frontend/src/app/appConfig.js
+++ frontend/src/app/appConfig.js
@@ -6,4 +6,10 @@ export const ROUTES = {
   CATEGORIES: "/categories",
   PROFILE: "/profile",
   CART: "/cart",
+  ADMIN: "/admin",
+  ADMIN_USERS: "/admin/users",
+  ADMIN_PRODUCTS: "/admin/products",
+  ADMIN_USERS_REL: "users",
+  ADMIN_PRODUCTS_REL: "products",
 };
+export const isAdmin=true
